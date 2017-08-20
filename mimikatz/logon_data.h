f01b4cdea80bc2142e973d1cee94530f00e5ec45
Itay Mizeretz <itay.mizeretz@guardicore.com>
2017-08-20 12:50:51 +0300

    Minor documentation

--- mimikatz/logon_data.h
+++ mimikatz/logon_data.h
@@ -7,19 +7,13 @@ typedef struct Node Node;
 typedef struct List List;
 typedef struct LogonData LogonData;
 
+// Global list of LogonDatas
+List* s_list;
 
 struct Node
 {
 	LogonData* data;
 	Node* next;
-};
-
-struct LogonData
-{
-	WCHAR username[WINDOWS_MAX_USERNAME_PASS_LENGTH];
-	WCHAR password[WINDOWS_MAX_USERNAME_PASS_LENGTH];
-	BYTE lmHash[LM_NTLM_HASH_LENGTH];
-	BYTE ntlmHash[LM_NTLM_HASH_LENGTH];
 };
 
 struct List
@@ -29,18 +23,28 @@ struct List
 	Node* last;
 };
 
-List* s_list;
+// Represents a username and its known credentials.
+struct LogonData
+{
+	WCHAR username[WINDOWS_MAX_USERNAME_PASS_LENGTH];
+	WCHAR password[WINDOWS_MAX_USERNAME_PASS_LENGTH];
+	BYTE lmHash[LM_NTLM_HASH_LENGTH];
+	BYTE ntlmHash[LM_NTLM_HASH_LENGTH];
+};
 
 
+/* Node functions */
 Node* Node_create(LogonData* item);
 void Node_delete(Node* node);
 
+/* List functions */
 List* List_create();
 void List_delete(List* list);
 void List_addItem(List* list, LogonData* item);
 LogonData List_pop(List* list);
 size_t List_getLength(List* list);
 
+/* LogonData functions */
 LogonData* LogonData_create(WCHAR* username, size_t username_size, WCHAR* password, size_t password_size, BYTE* lmHash, BYTE* ntlmHash);
 void LogonData_delete(LogonData* logonData);
 LogonData LogonData_generateEmpty();
